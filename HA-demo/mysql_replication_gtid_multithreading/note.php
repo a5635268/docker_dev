0c78ecc2e8a3785d14e7082400770ca9d56b8915
zhouxiaogang <xiaogang.zhou@qq.com>
2019-08-09 18:36:48 +0800

    数据库高可用研究

--- HA-demo/mysql_replication_gtid_multithreading/note.php
+++ HA-demo/mysql_replication_gtid_multithreading/note.php
@@ -1,14 +1,6 @@
-edu-mysql-bin.000001 |      618
 
-MySQL 主从数据同步延迟问题的调优
+wget http://dl.mycat.io/1.6.7.1/Mycat-server-1.6.7.1-release-20190627191042-linux.tar.gz
 
+# 注意C是大写
+tar zxvf ./Mycat-server-1.6.7.1-release-20190627191042-linux.tar.gz  -C /usr/local/
 
-mysql> grant replication slave on *.* to 'repl'@'172.31.0.%' identified by '123456';
-mysql> flush privileges;
-
-
-mysql> CHANGE MASTER TO MASTER_HOST='172.31.0.2',MASTER_USER='repl',MASTER_PASSWORD='123456',MASTER_AUTO_POSITION=1;
-mysql> START SLAVE;
-
-Retrieved_Gtid_Set: 1b74566f-9707-11e9-b449-0242ac1f0002:1-27
-Executed_Gtid_Set: 1b74566f-9707-11e9-b449-0242ac1f0002:1-27
